7c64e2cd8212fca918322e3a7580b739350d7863
Gabriel Rocha <roochagaabriel@gmail.com>
2021-03-17 20:39:32 -0300

    commit de att

--- aulas/backend/index.js
+++ aulas/backend/index.js
@@ -2,7 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
+/**  
+ * rota / recursos
+*/
+
+/**
+ * Métodos HTTP;
+ * 
+ * GET: Buscar uma informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informção no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+
+app.post('/users', (request,response) => {
   return response.json({
     evento: 'Semana Omnistack 11.0',
     aluno: 'Gabriel Rocha'
